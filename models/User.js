f765eadaab16ae1fd89be3a589e8027abc365b89
sesco001 <marcasmatheka@gmail.com>
2025-09-25 04:29:52 -0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+import bcrypt from "bcryptjs";
+
+const User = sequelize.define("User", {
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, defaultValue: "user" },
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
+});
+
+User.prototype.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default User;
